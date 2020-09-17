050702a378f2b90a93b42448a3d2f9ebd44231d9
Kneasle <kneasle@gmail.com>
2020-09-17 21:49:18 +0100

    Allow formatting ASTs in different styles

--- src/ast/json.rs
+++ src/ast/json.rs
@@ -1,5 +1,15 @@
 use crate::ast::AST;
 
+/// An enum to hold the different ways that a JSON AST can be formatted
+#[derive(Eq, PartialEq, Copy, Clone)]
+pub enum JSONFormat {
+    /// The most compact representation, has minimal whitespace.
+    /// E.g. `[{"foo": true, "bar: false}, true]`
+    Compact,
+    /// A prettified representation, with pretty indenting and every element on a newline.
+    Pretty,
+}
+
 /// The sapling representation of the AST for a subset of JSON (where all values are either 'true'
 /// or 'false', and keys only contain ASCII).
 #[derive(Eq, PartialEq, Clone)]
@@ -17,14 +27,8 @@ pub enum JSON {
     Object(Vec<(String, JSON)>),
 }
 
-impl Default for JSON {
-    fn default() -> JSON {
-        JSON::Object(vec![])
-    }
-}
-
-impl AST for JSON {
-    fn write_text(&self, string: &mut String) {
+impl JSON {
+    fn write_text_compact(&self, string: &mut String) {
         match self {
             JSON::True => {
                 string.push_str("true");
@@ -35,18 +39,16 @@ impl AST for JSON {
             JSON::Array(children) => {
                 // All arrays start with a '['
                 string.push('[');
-                // Push the first child (if it exists) without starting with a ','.
-                // We don't need to fuse 'child_iter' because std::slice::Iter is already a
-                // FusedIterator
-                let mut child_iter = children.iter();
-                if let Some(first_child) = child_iter.next() {
-                    first_child.write_text(string);
-                }
-                // Iterate over all the other children, and push a ', ' in front of them to make
-                // sure that the values are comma-delimited
-                for child in child_iter {
-                    string.push_str(", ");
-                    child.write_text(string);
+                // Append all the children, separated by commas
+                let mut is_first_child = true;
+                for child in children.iter() {
+                    // Add the comma if this isn't the first element
+                    if !is_first_child {
+                        string.push_str(", ");
+                    }
+                    is_first_child = false;
+                    // Push the field's name then a colon then the child value
+                    child.write_text_compact(string);
                 }
                 // Finish the array with a ']'
                 string.push(']');
@@ -54,28 +56,112 @@ impl AST for JSON {
             JSON::Object(fields) => {
                 // All objects start with a '{'
                 string.push('{');
-                // Push the first child (if it exists) without starting with a ','.
-                // We don't need to fuse 'child_iter' because std::slice::Iter is already a
-                // FusedIterator
-                let mut child_iter = fields.iter();
-                if let Some((name, child)) = child_iter.next() {
-                    // Push the field's name, and a colon
+                // Append all the children, separated by commas
+                let mut is_first_child = true;
+                for (name, child) in fields.iter() {
+                    // Add the comma if this isn't the first element
+                    if !is_first_child {
+                        string.push_str(", ");
+                    }
+                    is_first_child = false;
+                    // Push the field's name then a colon then the child value
                     string.push('"');
                     string.push_str(name);
                     string.push_str("\": ");
-                    child.write_text(string);
-                }
-                // Iterate over all the other fields, and push a ', ' in front of them to make
-                // sure that the values are comma-delimited
-                for (name, child) in child_iter {
-                    string.push_str(", ");
-                    string.push('"');
-                    string.push_str(name);
-                    string.push_str("\": ");
-                    child.write_text(string);
+                    child.write_text_compact(string);
                 }
                 // Finish the array with a '}'
                 string.push('}');
+            }
+        }
+    }
+
+    fn write_text_pretty(&self, string: &mut String, indentation_buffer: &mut String) {
+        // Insert the text for this JSON tree
+        match self {
+            JSON::True => {
+                string.push_str("true");
+            }
+            JSON::False => {
+                string.push_str("false");
+            }
+            JSON::Array(children) => {
+                // Push the '[' on its own line
+                string.push_str("[\n");
+                // Indent by one extra level
+                indentation_buffer.push_str("    ");
+                // Append all the children, separated by commas
+                let mut is_first_child = true;
+                for child in children.iter() {
+                    // Add the comma if this isn't the first element
+                    if !is_first_child {
+                        string.push_str(",\n");
+                    }
+                    is_first_child = false;
+                    // Indent and then render the child
+                    string.push_str(indentation_buffer);
+                    child.write_text_pretty(string, indentation_buffer);
+                }
+                // Return to the current indentation level
+                for _ in 0..4 {
+                    indentation_buffer.pop();
+                }
+                // Put the finishing ']' on its own line
+                string.push('\n');
+                string.push_str(indentation_buffer);
+                string.push(']');
+            }
+            JSON::Object(fields) => {
+                // Push the '{' on its own line
+                string.push_str("{\n");
+                // Indent by one extra level
+                indentation_buffer.push_str("    ");
+                // Append all the children, separated by commas
+                let mut is_first_child = true;
+                for (name, child) in fields.iter() {
+                    // Add the comma if this isn't the first element
+                    if !is_first_child {
+                        string.push_str(",\n");
+                    }
+                    is_first_child = false;
+                    // Indent the right number of times
+                    string.push_str(indentation_buffer);
+                    // Push the field's name then a colon then the child value
+                    string.push('"');
+                    string.push_str(name);
+                    string.push_str("\": ");
+                    child.write_text_pretty(string, indentation_buffer);
+                }
+                // Return to the current indentation level
+                for _ in 0..4 {
+                    indentation_buffer.pop();
+                }
+                // Put the finishing '}' on its own line
+                string.push('\n');
+                string.push_str(indentation_buffer);
+                string.push('}');
+            }
+        }
+    }
+}
+
+impl Default for JSON {
+    fn default() -> JSON {
+        JSON::Object(vec![])
+    }
+}
+
+impl AST for JSON {
+    type FormatStyle = JSONFormat;
+
+    fn write_text(&self, string: &mut String, format_style: JSONFormat) {
+        match format_style {
+            JSONFormat::Compact => {
+                self.write_text_compact(string);
+            }
+            JSONFormat::Pretty => {
+                let mut indentation_buffer = String::new();
+                self.write_text_pretty(string, &mut indentation_buffer);
             }
         }
     }
@@ -83,10 +169,10 @@ impl AST for JSON {
 
 #[cfg(test)]
 mod tests {
-    use super::{AST, JSON};
+    use super::{JSONFormat, AST, JSON};
 
     #[test]
-    fn text_conversion() {
+    fn to_text_compact() {
         for (tree, expected_string) in &[
             (JSON::True, "true"),
             (JSON::False, "false"),
@@ -112,7 +198,57 @@ mod tests {
                 r#"[{"foos": [false, true, false], "bar": false}, true]"#,
             ),
         ] {
-            assert_eq!(tree.to_text(), *expected_string);
+            assert_eq!(tree.to_text(JSONFormat::Compact), *expected_string);
+        }
+    }
+
+    #[test]
+    fn to_text_pretty() {
+        for (tree, expected_string) in &[
+            (JSON::True, "true"),
+            (JSON::False, "false"),
+            (
+                JSON::Array(vec![JSON::True, JSON::False]),
+                "[
+    true,
+    false
+]",
+            ),
+            (
+                JSON::Object(vec![
+                    ("foo".to_string(), JSON::True),
+                    ("bar".to_string(), JSON::False),
+                ]),
+                r#"{
+    "foo": true,
+    "bar": false
+}"#,
+            ),
+            (
+                JSON::Array(vec![
+                    JSON::Object(vec![
+                        (
+                            "foos".to_string(),
+                            JSON::Array(vec![JSON::False, JSON::True, JSON::False]),
+                        ),
+                        ("bar".to_string(), JSON::False),
+                    ]),
+                    JSON::True,
+                ]),
+                r#"[
+    {
+        "foos": [
+            false,
+            true,
+            false
+        ],
+        "bar": false
+    },
+    true
+]"#,
+            ),
+        ] {
+            assert_eq!(tree.to_text(JSONFormat::Pretty), *expected_string);
         }
     }
 }
--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -4,14 +4,16 @@ pub mod json;
 
 /// The specification of an AST that sapling can edit
 pub trait AST: Eq + Default {
+    type FormatStyle;
+
     /// Write the textual representation of an AST to a string
-    fn write_text(&self, string: &mut String);
+    fn write_text(&self, string: &mut String, format_style: Self::FormatStyle);
 
     /// Make a [String] representing this AST.  Same as [write_text](AST::write_text) but creates a
     /// new [String].
-    fn to_text(&self) -> String {
+    fn to_text(&self, format_style: Self::FormatStyle) -> String {
         let mut s = String::new();
-        self.write_text(&mut s);
+        self.write_text(&mut s, format_style);
         s
     }
 }
