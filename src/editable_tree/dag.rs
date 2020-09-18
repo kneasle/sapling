5404f23dbd65aa38794be46db600b74a0783352a
Kneasle <kneasle@gmail.com>
2020-09-18 22:14:35 +0100

    Add doc comment for `DAG`

--- src/editable_tree/dag.rs
+++ src/editable_tree/dag.rs
@@ -2,6 +2,11 @@ use super::reference::Ref;
 use super::EditableTree;
 use crate::ast_spec::{ASTSpec, NodeMap};
 
+/// An [EditableTree] that stores the history as a DAG (Directed Acyclic Graph).  This means that
+/// every node that has ever been created exists somewhere in the DAG, and when changes are made,
+/// every node above that is cloned until the root is reached and that root becomes the new root.
+/// Therefore, moving back through the history is as simple as reading a different root node from
+/// the `roots` vector, and following its descendants through the DAG of nodes.
 #[derive(Debug, Clone)]
 pub struct DAG<Node: ASTSpec<Ref>> {
     nodes: Vec<Node>,
