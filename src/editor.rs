8272764fa435aa6fd1924a97df179cffeecdd716
Kneasle <kneasle@gmail.com>
2020-09-21 16:23:21 +0100

    Add interpreting function for commands

--- src/editor.rs
+++ src/editor.rs
@@ -2,6 +2,48 @@ use crate::ast_spec::{ASTSpec, Reference};
 use crate::editable_tree::EditableTree;
 use tuikit::prelude::*;
 
+/// The possible outcomes of a user-typed command
+#[derive(Debug, Clone, Eq, PartialEq)]
+enum Action {
+    /// The user typed a command that isn't defined, but the command box should still be cleared
+    Undefined,
+    /// Quit Sapling
+    Quit,
+    /// Replace the currently selected node with a node represented by some [char]
+    Replace(char),
+}
+
+/// Attempt to convert a command as a [str] into an [Action].
+/// This parses the string from the start, and returns when it finds a valid command.
+/// Therefore, `"q489flshb"` will be treated like `"q"`, and will return `Some(Action::Quit)`.
+/// This returns:
+/// - [None] if the command is incomplete.
+/// - [Action::Undefined] if the command is not defined (like the command "X").
+/// - The corresponding [Action], otherwise.
+fn interpret_command(command: &str) -> Option<Action> {
+    let mut command_char_iter = command.chars();
+
+    // Consume the first char of the command
+    if let Some(c) = command_char_iter.next() {
+        match c {
+            // "q" quits Sapling
+            'q' => {
+                return Some(Action::Quit);
+            }
+            'r' => {
+                // Consume the second char of the iterator
+                if let Some(replace_char) = command_char_iter.next() {
+                    return Some(Action::Replace(replace_char));
+                }
+            }
+            _ => {
+                return Some(Action::Undefined);
+            }
+        }
+    }
+
+    None
+}
 
 /// A struct to hold the top-level components of the editor.
 pub struct Editor<R: Reference, T: ASTSpec<R>, E: EditableTree<R, T>> {
@@ -32,12 +74,20 @@ impl<R: Reference, T: ASTSpec<R>, E: EditableTree<R, T>> Editor<R, T, E> {
 
             if let Event::Key(key) = event {
                 match key {
-                    // If the user types 'q' then quit the program
-                    Key::Char('q') => {
-                        break;
-                    }
                     Key::Char(c) => {
                         self.command.push(c);
+                        if let Some(action) = interpret_command(&self.command) {
+                            // Clear the command box
+                            self.command.clear();
+                            // Respond to the command
+                            match action {
+                                Action::Undefined => {}
+                                Action::Quit => {
+                                    break;
+                                }
+                                Action::Replace(_c) => {}
+                            }
+                        }
                     }
                     Key::ESC => {
                         self.command.clear();
@@ -66,6 +116,31 @@ impl<R: Reference, T: ASTSpec<R>, E: EditableTree<R, T>> Editor<R, T, E> {
                 .unwrap();
             // Update the terminal screen
             self.term.present().unwrap();
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{interpret_command, Action};
+
+    #[test]
+    fn interpret_command_complete() {
+        for (command, expected_effect) in &[
+            ("q", Action::Quit),
+            ("x", Action::Undefined),
+            ("pajlbsi", Action::Undefined),
+            ("ra", Action::Replace('a')),
+            ("rg", Action::Replace('g')),
+        ] {
+            assert_eq!(interpret_command(*command), Some(expected_effect.clone()));
+        }
+    }
+
+    #[test]
+    fn interpret_command_incomplete() {
+        for command in &["r"] {
+            assert_eq!(interpret_command(*command), None);
         }
     }
 }
