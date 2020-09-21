802de87d9cbc49e976ce0b0044f459f99069c2ca
Kneasle <kneasle@gmail.com>
2020-09-21 12:23:43 +0100

    Separate the traits EditableTree and NodeMap

--- src/editable_tree/dag.rs
+++ src/editable_tree/dag.rs
@@ -1,6 +1,6 @@
 use super::EditableTree;
 use crate::ast_spec::{ASTSpec, NodeMap};
-use crate::vec_node_map::Index;
+use crate::vec_node_map::{Index, VecNodeMap};
 
 /// An [EditableTree] that stores the history as a DAG (Directed Acyclic Graph).  This means that
 /// every node that has ever been created exists somewhere in the DAG, and when changes are made,
@@ -9,59 +9,21 @@ use crate::vec_node_map::Index;
 /// the `roots` vector, and following its descendants through the DAG of nodes.
 #[derive(Debug, Clone)]
 pub struct DAG<Node: ASTSpec<Index>> {
-    nodes: Vec<Node>,
+    node_map: VecNodeMap<Node>,
     roots: Vec<Index>,
-}
-
-impl<Node: ASTSpec<Index>> DAG<Node> {}
-
-impl<Node: ASTSpec<Index>> NodeMap<Index, Node> for DAG<Node> {
-    /// Create a new `NodeMap` with a given `Node` as root
-    fn with_root(root: Node) -> Self {
-        DAG {
-            nodes: vec![root],
-            roots: vec![Index::from(0)],
-        }
-    }
-
-    /// Get the reference of the root node of the tree
-    fn root(&self) -> Index {
-        // We can unwrap here because we uphold the invariant that there must always be at least
-        // one root in the history.
-        *self.roots.last().unwrap()
-    }
-
-    /// Get the reference of the root node of the tree
-    fn set_root(&mut self, new_root: Index) -> bool {
-        let is_ref_valid = self.get_node(new_root).is_some();
-        if is_ref_valid {
-            self.roots.push(new_root);
-        }
-        is_ref_valid
-    }
-
-    /// Gets node from a reference, returning [None] if the reference is invalid.
-    fn get_node<'a>(&'a self, id: Index) -> Option<&'a Node> {
-        self.nodes.get(id.as_usize())
-    }
-
-    /// Gets mutable node from a reference, returning [None] if the reference is invalid.
-    fn get_node_mut<'a>(&'a mut self, id: Index) -> Option<&'a mut Node> {
-        self.nodes.get_mut(id.as_usize())
-    }
-
-    /// Add a new `Node` to the tree, and return its reference
-    fn add_node(&mut self, node: Node) -> Index {
-        self.nodes.push(node);
-        Index::from(self.nodes.len() - 1)
-    }
 }
 
 impl<Node: ASTSpec<Index>> EditableTree<Index, Node> for DAG<Node> {
     fn new() -> Self {
+        let node_map = VecNodeMap::with_default_root();
         DAG {
-            nodes: vec![Node::default()],
-            roots: vec![Index::from(0)],
+            roots: vec![node_map.root()],
+            node_map,
         }
+    }
+
+    /// Build the text representation of the current tree into the given [String]
+    fn write_text(&self, string: &mut String, format: &Node::FormatStyle) {
+        self.node_map.write_text(string, format);
     }
 }
--- src/editable_tree/mod.rs
+++ src/editable_tree/mod.rs
@@ -1,8 +1,18 @@
 pub mod dag;
 
-use crate::ast_spec::{ASTSpec, NodeMap, Reference};
+use crate::ast_spec::{ASTSpec, Reference};
 
-pub trait EditableTree<Ref: Reference, Node: ASTSpec<Ref>>: NodeMap<Ref, Node> + Sized {
-    /// Build a new EditableTree with a default AST
+pub trait EditableTree<Ref: Reference, Node: ASTSpec<Ref>>: Sized {
+    /// Build a new EditableTree with the default AST of the given type
     fn new() -> Self;
+
+    /// Build the text representation of the current tree into the given [String]
+    fn write_text(&self, string: &mut String, format: &Node::FormatStyle);
+
+    /// Build and return a [String] of the current tree
+    fn to_text(&self, format: &Node::FormatStyle) -> String {
+        let mut s = String::new();
+        self.write_text(&mut s, format);
+        s
+    }
 }
