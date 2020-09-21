8a4af05ef81c512e100a58874d2034a28e57d2a2
Kneasle <kneasle@gmail.com>
2020-09-21 17:10:35 +0100

    Refactor rendering code into its own function

--- src/editor.rs
+++ src/editor.rs
@@ -65,11 +65,33 @@ impl<R: Reference, T: ASTSpec<R>, E: EditableTree<R, T>> Editor<R, T, E> {
         }
     }
 
+    /// Update the terminal UI display
+    fn update_display(&self) {
+        // Put the terminal size into some convenient variables
+        let (width, height) = self.term.term_size().unwrap();
+
+        self.term.clear().unwrap();
+        // Print the AST to the terminal
+        self.term
+            .print(0, 0, &self.tree.to_text(&self.format_style))
+            .unwrap();
+        // Render the bottom bar of the editor
+        self.term
+            .print(height - 1, 0, "Press 'q' to exit.")
+            .unwrap();
+        self.term
+            .print(
+                height - 1,
+                width - 5 - self.command.chars().count(),
+                &self.command,
+            )
+            .unwrap();
+        // Update the terminal screen
+        self.term.present().unwrap();
+    }
+
     pub fn mainloop(mut self) {
         while let Ok(event) = self.term.poll_event() {
-            // Put the terminal size into some convenient variables
-            let (width, height) = self.term.term_size().unwrap();
-
             /* RESPOND TO THE USER'S INPUT */
 
             if let Event::Key(key) = event {
@@ -99,26 +121,10 @@ impl<R: Reference, T: ASTSpec<R>, E: EditableTree<R, T>> Editor<R, T, E> {
                 }
             }
 
-            /* RENDER THE EDITOR UI */
-
-            self.term.clear().unwrap();
-            // Print the AST to the terminal
-            self.term
-                .print(0, 0, &self.tree.to_text(&self.format_style))
-                .unwrap();
-            // Render the bottom bar of the editor
-            self.term
-                .print(height - 1, 0, "Press 'q' to exit.")
-                .unwrap();
-            self.term
-                .print(
-                    height - 1,
-                    width - 5 - self.command.chars().count(),
-                    &self.command,
-                )
-                .unwrap();
-            // Update the terminal screen
-            self.term.present().unwrap();
+            // Update the screen after every update (if this becomes a bottleneck then we can
+            // optimise the number of calls to `update_display` but for now it's not worth the
+            // added complexity)
+            self.update_display();
         }
     }
 }
