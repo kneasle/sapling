5d2a4ec200a0bab5debfdb31f1dfad70bb69a157
Kneasle <kneasle@gmail.com>
2020-09-21 16:00:06 +0100

    Add some section comments

--- src/editable_tree/mod.rs
+++ src/editable_tree/mod.rs
@@ -3,8 +3,12 @@ pub mod dag;
 use crate::ast_spec::{ASTSpec, Reference};
 
 pub trait EditableTree<Ref: Reference, Node: ASTSpec<Ref>>: Sized {
+    /* CONSTRUCTOR METHODS */
+
     /// Build a new EditableTree with the default AST of the given type
     fn new() -> Self;
+
+    /* DISPLAY METHODS */
 
     /// Build the text representation of the current tree into the given [String]
     fn write_text(&self, string: &mut String, format: &Node::FormatStyle);
