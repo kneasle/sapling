6b55a095824636227f5e8fedadd5143c4358c12a
Kneasle <kneasle@gmail.com>
2021-09-30 00:56:54 +0100

    Add struct to store cursor locations

--- sapling/src/lib.rs
+++ sapling/src/lib.rs
@@ -1,4 +1,7 @@
-pub mod ast;
-mod lang;
+//! Front-end- and language-independent code for the Sapling **editor**.
+
+pub mod ast; // Tree storage
+mod cursors; // Cursor location
+mod lang; // Language definition
 
 pub use lang::Lang;
